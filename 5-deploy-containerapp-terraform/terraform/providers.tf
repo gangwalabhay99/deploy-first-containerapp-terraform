242c2cd33c9609f554263e929db38a1f8687a580
gangwalabhay99 <83031013+gangwalabhay99@users.noreply.github.com>
2023-05-06 22:44:53 +0530

    Update providers.tf

--- 5-deploy-containerapp-terraform/terraform/providers.tf
+++ 5-deploy-containerapp-terraform/terraform/providers.tf
@@ -4,8 +4,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "deploy-first-containerapp-rg"
-    storage_account_name = "deployfirstcontainersa"
+    resource_group_name  = "deploy-first-containerapp-g"
+    storage_account_name = "deployfirstcontiners121"
     container_name       = "tfstate"
     key                  = "terraform-containerapp.tfstate"
   }
