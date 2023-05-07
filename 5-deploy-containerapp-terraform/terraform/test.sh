3e3f614c15d5eddd0e277b45960c036df017f00d
gangwalabhay99 <83031013+gangwalabhay99@users.noreply.github.com>
2023-05-08 02:36:18 +0530

    Update test.sh

--- 5-deploy-containerapp-terraform/terraform/test.sh
+++ 5-deploy-containerapp-terraform/terraform/test.sh
@@ -4,4 +4,5 @@ az containerapp create --name "containertam" \
   --user-assigned "/subscriptions/35f15df1-41f7-404e-9898-8ac5b582ddc1/resourcegroups/firstcontainerapp283-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/containerappmi" \
   --registry-identity "/subscriptions/35f15df1-41f7-404e-9898-8ac5b582ddc1/resourcegroups/firstcontainerapp283-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/containerappmi" \
   --registry-server "firstcontainerapp283acracr.azurecr.io" \
-  --image "firstcontainerapp283acracr.azurecr.io/aspcoresample:58533d062c15af417615e9866e37dcaa36f49845"
+  --image "firstcontainerapp283acracr.azurecr.io/aspcoresample:817851e372d34809368298f0cf15e2e91054355d
+"
