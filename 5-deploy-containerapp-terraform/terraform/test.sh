950e9d0d99b73a19b641d811dbe6e6c07349ca33
gangwalabhay99 <83031013+gangwalabhay99@users.noreply.github.com>
2023-05-08 02:02:32 +0530

    Update test.sh

--- 5-deploy-containerapp-terraform/terraform/test.sh
+++ 5-deploy-containerapp-terraform/terraform/test.sh
@@ -1,7 +1,7 @@
 az containerapp create --name "containertam" \
-  --resource-group "firstcontainerapp-rg" \
+  --resource-group "firstcontainerapp283-rg" \
   --environment "firstcontainerappacaenv" \
-  --user-assigned "/subscriptions/04109105-f3ca-44ac-a3a7-66b4936112c3/resourcegroups/firstcontainerapp-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/containerappmi" \
-  --registry-identity "/subscriptions/04109105-f3ca-44ac-a3a7-66b4936112c3/resourcegroups/firstcontainerapp-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/containerappmi" \
-  --registry-server "firstcontainerappacracr.azurecr.io" \
-  --image "firstcontainerappacracr.azurecr.io/aspcoresample:58533d062c15af417615e9866e37dcaa36f49845"
+  --user-assigned "/subscriptions/35f15df1-41f7-404e-9898-8ac5b582ddc1/resourcegroups/firstcontainerapp283-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/containerappmi" \
+  --registry-identity "/subscriptions/35f15df1-41f7-404e-9898-8ac5b582ddc1/resourcegroups/firstcontainerapp283-rg/providers/Microsoft.ManagedIdentity/userAssignedIdentities/containerappmi" \
+  --registry-server "firstcontainerapp283acracr.azurecr.io" \
+  --image "firstcontainerapp283acracr.azurecr.io/aspcoresample:58533d062c15af417615e9866e37dcaa36f49845"
