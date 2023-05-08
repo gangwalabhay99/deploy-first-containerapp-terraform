6fbe020a20a7aee6b62d198972b39af3fe229f94
gangwalabhay99 <83031013+gangwalabhay99@users.noreply.github.com>
2023-05-08 10:12:49 +0530

    Update acr.tf

--- 3-deploy-terraform-base/terraform/modules/acr/acr.tf
+++ 3-deploy-terraform-base/terraform/modules/acr/acr.tf
@@ -1,6 +1,6 @@
 resource "azurerm_container_registry" "acr" {
   name                = "${var.name}acr"
-  resource_group_name = var.resource_group_name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "Premium"
   admin_enabled       = true
