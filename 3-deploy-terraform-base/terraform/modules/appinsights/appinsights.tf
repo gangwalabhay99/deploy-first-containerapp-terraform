b13a912b5519df2eeaa9840b4dfdf961b06cfe88
gangwalabhay99 <83031013+gangwalabhay99@users.noreply.github.com>
2023-05-09 18:35:39 +0530

    Update appinsights.tf

--- 3-deploy-terraform-base/terraform/modules/appinsights/appinsights.tf
+++ 3-deploy-terraform-base/terraform/modules/appinsights/appinsights.tf
@@ -1,6 +1,6 @@
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.name}-rg"
   location            = var.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+  resource_group_name = var.resource_group_name
   application_type    = var.application_type
 }
